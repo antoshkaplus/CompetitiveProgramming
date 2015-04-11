d006285264c8286896d55dbd54bf2a16a732390e
Anton Logunov <antoshkaplus@gmail.com>
2015-04-11 06:33:35 -0400

    m

--- CodeJam/2015/b.py
+++ CodeJam/2015/b.py
@@ -1,11 +1,17 @@
 
-from heapq import *
+from heapq import * 
 
 T = int(raw_input())
-D = int(raw_input())
-ps = map(int, raw_input().split())
-res = -ps[0]
-for i in range(res):
-    r = heappop(ps)
-    if r > res:
-        res = r
+for t in range(T):
+    D = int(raw_input())
+    ps = map(lambda x: -int(x), raw_input().split())
+    heapify(ps)
+    res = -ps[0]
+    for i in range(res):
+        r = -heappop(ps)
+        if r + i < res:
+            res = r + i
+        a = r/2
+        heappush(ps, -a)
+        heappush(ps, -(r - a))
+    print "Case #" + str(t+1) + ":", res
