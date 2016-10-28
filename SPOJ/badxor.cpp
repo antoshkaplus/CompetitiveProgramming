ac9a78cf5abf29b4ceb86d2d50c07496bcca005d
Anton Logunov <antoshkaplus@gmail.com>
2016-10-28 07:44:06 -0400

    SPOJ baby, badxor

---
+++ SPOJ/badxor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <limits>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <bitset>
+
+using namespace std;
+
+// last position is just for XOR operation.
+// everything past this position can be written to last one
+int N_MAX = 11;
+
+
+// we init with 0
+array<int, 1 << N_MAX> t;
+
+int main(int argc, char **argv) {
+    std::ios_base::sync_with_stdio(false);
+    
+    
+    
+    
+}
